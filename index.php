2b91ca033c7d51e27fe023301a9d4f69be88d2b5
MargheritaMottana <margherita.mottana@gmail.com>
2024-10-03 19:44:53 +0200

    index: - creata la grafica in cui far comparire le card - aggiunto cilco per lettura dati delle card - aggiunto file css per un poco di stile

--- index.php
+++ index.php
@@ -9,20 +9,47 @@
     <!-- bs -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
+    <!-- css -->
+     <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
 
-    <header class="container pt-5">
-        <h1>
+    <header class="container py-3 text-center">
+        <h1 class="text-white">
             PHP Dischi JSON
         </h1>
     </header>
 
     <main id="app" class="container pb-5">
-        <p>
-            {{message}}
-        </p>
+
+        <div class="row">
+            <div class="col-4 mb-4" v-for="disc in discs">
+
+                <div class="card rounded-4 p-2 border-0">
+                    <img :src="disc.poster" class="card-img-top rounded-top-4 p-2" :alt="disc.title">
+
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            {{ disc.title }}
+                        </h3>
+                        <h5 class="card-text">
+                            {{ disc.genre }}
+                        </h5>
+                        <p class="card-text">
+                            {{ disc.year }}
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
     </main>
 
     <!-- bs -->
